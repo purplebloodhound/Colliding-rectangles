1807b1e7452d19ee9f6a1a49775f4c834a52dad1
purplebloodhound <70716009+purplebloodhound@users.noreply.github.com>
2020-09-03 10:28:53 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
+var fixedrect, movingrect;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1600,800);
+  fixedrect = createSprite(800, 400, 80, 80);
+  movingrect = createSprite(50, 100, 80, 50);
 }
 
 function draw() {
   background(255,255,255);  
+  mixedrect.x = World.mouseX;
+  mixedrect.y = World.mouseY;
   drawSprites();
 }
